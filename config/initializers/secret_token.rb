30a8a205e087410a715132438d08609bbdc1e0cc
Andre Carvalho <andrecarva@gmail.com>
2013-08-11 18:22:33 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Guiadatv::Application.config.secret_key_base = '461b7dc13d40dd722ff9be32da27f6b2db34fb22c28fc29776a4711e23d575d80c918b6553aafdf5abb84de922f24c3f301ad0fabbfc5155f5559333edadf288'
